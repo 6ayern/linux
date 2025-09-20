7a2bcf4cfeab32d3d0df732fe78c5f2733fee2a1
Alina <anitalozova@gmail.com>
2025-09-20 13:47:50 +0300

    1

--- kubsh.c
+++ kubsh.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
- printf("$");
+int main(){
+char input[100];
+while(1){
+	fgets(input, 100, stdin);
+	input[strlen(input) - 1] = '\0';
+	if(strcmp(input, "\\q") == 0){
+		break;
+	}
+ 	printf("%s: command not foud\n", input);
+}
 
- char input[100];
- fgets(input, 100, stdin);
-
- input[strlen(input) - 1] = '\0';
- printf("%s: command not found\n", input);
  return 0;
 }
