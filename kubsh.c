54e5b45c22af57703d1f6a905581c7ec755f8f3c
Alina <anitalozova@gmail.com>
2025-09-27 13:51:06 +0300

    123

--- kubsh.c
+++ kubsh.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+
+void debug(char line[100]){
+printf("%s\n", line);
+}
 
 int main(){
 char input[100];
 while(1){
 	fgets(input, 100, stdin);
 	input[strlen(input) - 1] = '\0';
-	if(strcmp(input, "\\q") == 0){
+	if(strcmp(input, "\\q") == 0) {
 		break;
 	}
- 	printf("%s: command not foud\n", input);
-}
-
+	else if(strncmp(input, "debug ", 5)==0) {
+	debug(input);
+	} else{
+ 	printf("%s: command not found\n", input);
+	}
  return 0;
+}
 }
