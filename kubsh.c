dda4a7e557a7798797aa0d257a540fcf98683187
Alina <anitalozova@gmail.com>
2025-10-04 13:51:49 +0300

    123

--- kubsh.c
+++ kubsh.c
@@ -2,27 +2,47 @@
 #include <stdbool.h>
 #include <string.h>
 #include<readline/readline.h>
+#include<readline/history.h>
+#include<signal.h>
+#define HISTORY_FILE ".kubsh_history"
+sig_atomic_t signal_received = 0;
+
 void debug(char *line){
-printf("%s\n", line);
+	printf("%s\n", line);
 }
+
+void sig_handler(int signum) {
+ signal_received = signum;
+ printf("Configuration reloaded");
+}
+
 int main(){
-char *input;
-while(true){
-	input = readline("$ ");
-	if(input == NULL || *input =='\0'){
-	free(input);
-	break;
-}
-	if(!strcmp(input, "\\q")) {
-		break;
-	}
-	else if(strncmp(input, "debug ", 5)==0) {
-	debug(input);
-	} else{
- 	printf("%s: command not found\n", input);
-	}
-free(input);
-}
+ rl_clear_signals;
+ signal(SIGINT, sig_handler);
+ read_history(HISTORY_FILE);
+ char *input;
+ while(true){
+  input = readline("$ ");
+
+  if(signal_received) {
+   signal_received = 0;
+   continue;
+   }
+   if(input == NULL){
+    break;
+   }
+   add_history(input);
+   if(!strcmp(input, "\\q")) {
+    break;
+   }
+   else if(strncmp(input, "debug ", 5)==0) {
+    debug(input);
+    } else{
+     printf("%s: command not found\n", input);
+     }
+   free(input);
+   }
+ write_history(HISTORY_FILE);
  return 0;
 }
 
